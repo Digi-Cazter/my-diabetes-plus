2133b2774dc761751d8d4787430f8e4f0be33816
Donavan White <digi.cazter@gmail.com>
2013-11-15 09:27:54 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyDiabetesPlus::Application.config.secret_key_base = 'a890126a643581c40977aecf6f335f922e43492bed29e2d7060f844bd57ed4a9354dc9eaf87a566851bf525de52d8a81fb9c25e79fc8b335161a9a022180c5d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyDiabetesPlus::Application.config.session_store :cookie_store, key: '_my-diabetes-plus_session'
